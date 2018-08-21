58c3132837316cbed93d13c35b990d722c49a44f
tikinson <42579557+tikinson@users.noreply.github.com>
2018-08-21 18:02:04 +0300

    Add files via upload

---
+++ coll_stepping_digita_sensl.ino.ino
@@ -0,0 +1,71 @@
+int enA = 10;
+int in1 = 6;
+int in2 = 5;
+int sensVals[2];
+
+
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(enA, OUTPUT); 
+ pinMode(in1, OUTPUT);
+ pinMode(in2, OUTPUT);
+ 
+ pinMode(8,INPUT);
+ digitalWrite(8, LOW);
+ pinMode(4,INPUT);
+ digitalWrite(4, LOW);
+}
+
+
+void movingDrive() {
+sensVals[0]= digitalRead(4);
+sensVals[1]= digitalRead(8);
+
+
+ //digitalWrite(in1, LOW);
+ //digitalWrite(in2, LOW);
+ //analogWrite (enA, 0);
+  
+for ( int i = 0; i <= 2; i++) {
+  Serial.print( "s1 :");
+  Serial.println (sensVals[0]);
+  Serial.print( "s2 :");
+  Serial.println (sensVals[1]);
+ 
+if  (sensVals[0] ==0 && sensVals[1] == 1) {
+   digitalWrite(in1, LOW);
+   digitalWrite(in2, HIGH);
+   analogWrite(enA, 255);
+   delay(20);
+   Serial.print( " moving right " );
+}
+
+else if (sensVals[0] == 1 && sensVals[1] == 0) {
+   digitalWrite(in1, HIGH);
+   digitalWrite(in2, LOW);
+   analogWrite(enA, 255);
+   delay(20);
+   Serial.print( " moving left ");
+}
+
+else {
+   digitalWrite(in1, LOW);
+   digitalWrite(in2, LOW);
+   analogWrite(enA, 255);
+   delay(20);
+   Serial.print( " stop moving ");
+}
+
+}
+
+}
+
+void loop() {
+ movingDrive();
+ delay(1000);
+ //8 минут = 480000 мс
+}
+
+
+
---
+++ stepping_test_2.ino
@@ -0,0 +1,101 @@
+int enA = 10;
+int enB = 9;
+int in1A = 6;
+int in1B = 5;
+int in2A = 4;
+int in2B = 3;
+int sensVals[4];
+
+
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(enA, OUTPUT); 
+ pinMode(in1A, OUTPUT);
+ pinMode(in2B, OUTPUT);
+ pinMode(enB, OUTPUT); 
+ pinMode(in2A, OUTPUT);
+ pinMode(in2B, OUTPUT);
+ 
+ pinMode(8,INPUT);
+ digitalWrite(8, LOW);
+ pinMode(4,INPUT);
+ digitalWrite(4, LOW);
+ 
+ pinMode(2,INPUT);
+ digitalWrite(2, LOW);
+ pinMode(11,INPUT);
+ digitalWrite(11, LOW);
+}
+
+
+void movingDrive() {
+sensVals[0]= digitalRead(4);
+sensVals[1]= digitalRead(8);
+sensVals[2]= digitalRead(2);
+sensVals[3]= digitalRead(1);
+
+
+  
+for ( int i = 0; i <= 2; i++) {
+  Serial.print( "s1 :");
+  Serial.println (sensVals[0]);
+  Serial.print( "s2 :");
+  Serial.println (sensVals[1]);
+  Serial.print( "s3 :");
+  Serial.println (sensVals[2]);
+  Serial.print( "s4 :");
+  Serial.println (sensVals[3]);
+ 
+if  (sensVals[0] ==0 && sensVals[1] == 1) {
+   digitalWrite(in1A, LOW);
+   digitalWrite(in1B, HIGH);
+   analogWrite(enA, 255);
+   delay(20);
+   Serial.print( " moving up " );
+}
+
+else if (sensVals[0] == 1 && sensVals[1] == 0) {
+   digitalWrite(in1A, HIGH);
+   digitalWrite(in1B, LOW);
+   analogWrite(enA, 255);
+   delay(20);
+   Serial.print( " moving down ");
+}
+else if (sensVals[2] == 1 && sensVals[3] == 0) {
+   digitalWrite(in2A, HIGH);
+   digitalWrite(in2B, LOW);
+   analogWrite(enB, 255);
+   delay(20);
+   Serial.print( " moving right ");
+}
+else if (sensVals[2] == 0 && sensVals[3] == 1) {
+   digitalWrite(in2A, LOW);
+   digitalWrite(in2B, HIGH);
+   analogWrite(enB, 255);
+   delay(20);
+   Serial.print( " moving left ");
+}
+
+else {
+   digitalWrite(in1A, LOW);
+   digitalWrite(in1B, LOW);
+   analogWrite(enA, 255);
+   digitalWrite(in2A, LOW);
+   digitalWrite(in2B, LOW);
+   analogWrite(enB, 255);
+   delay(20);
+   Serial.print( " stop moving ");
+}
+
+}
+
+}
+
+void loop() {
+ movingDrive();
+ delay(200);
+}
+
+
+
